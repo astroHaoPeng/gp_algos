1503bfbd4ddac1063eead5dffca6eef7817fe5ae
Michal Jamroz <mjamroz90@gmail.com>
2014-03-30 18:17:46 +0200

    Some code testing kalman filter in object tracking task, some improvements/fixes.

--- src/main/scala/dynamicalsystems/filtering/KalmanFilter.scala
+++ src/main/scala/dynamicalsystems/filtering/KalmanFilter.scala
@@ -10,58 +10,99 @@ class KalmanFilter {
   import KalmanFilter._
   import utils.StatsUtils._
 
-  def inferHiddenState(filteringInput:FilteringInput,optionalInput:Option[OptionalInput],computeLL:Boolean):FilteringOutput = {
+  def inferHiddenState(filteringInput:FilteringInput,
+					   optionalInput:Option[OptionalInput],computeLL:Boolean):FilteringOutput = {
+
+	filteringInput.transMatrices match {
+	  case Left(transitionMatrices) => inferHiddenStateNonStat(filteringInput,optionalInput,computeLL)
+	  case Right(stationaryTransMatrices) =>
+		val nonStatTransMatrices = stationaryModelToSeq(filteringInput.observations.cols,
+		stationaryTransMatrices)
+		inferHiddenStateNonStat(filteringInput.copy(transMatrices = Left(nonStatTransMatrices)),optionalInput,computeLL)
+	}
+  }
+
+  //TODO validate matrices dimensions
+  private def inferHiddenStateNonStat(filteringInput:FilteringInput,optionalInput:Option[OptionalInput],computeLL:Boolean):FilteringOutput = {
 
 	val y = filteringInput.observations
-	val (obsSpaceSize,tMax,hiddenSpaceSize) = (y.rows,y.cols,filteringInput.A(1).rows)
+	val (obsSpaceSize,tMax,hiddenSpaceSize) = (y.rows,y.cols,filteringInput.transMatrices.left.get.A(1).rows)
 	val (hiddenMeans,hiddenCovs) = (DenseMatrix.zeros[Double](hiddenSpaceSize,tMax),new Array[transitionMatrix](tMax))
 
 	val optInput = optionalInput match {
 	  case Some(input) => input
 	  case None =>
-		val zeroTransitionMatrix = DenseMatrix.zeros[Double](hiddenSpaceSize,1)
-		val arrWithZerosTransitionMatrix = new Array[transitionMatrix](tMax)
-		(0 until tMax).foreach{index => arrWithZerosTransitionMatrix(index) = zeroTransitionMatrix}
-		val (u,bMatrix,dMatrix) = (DenseMatrix.zeros[Double](tMax,1),arrWithZerosTransitionMatrix,arrWithZerosTransitionMatrix)
-		OptionalInput(B = bMatrix,D = dMatrix,u = u)
+		generateOptionalInput(tMax,hiddenSpaceSize,obsSpaceSize)
 	}
-
+	var logLikelihood:Option[Double] = if (computeLL){Some(0.)} else {None}
 	for (t <- (0 until tMax).toStream){
 	  val (prevM,prevCov) = (t == 0) match {
 		case true => (filteringInput.initMean,filteringInput.initCov)
 		case false => (hiddenMeans(::,t-1),hiddenCovs(t-1))
 	  }
-	  val (inferredMean,inferredCov,_) = kalmanUpdate(filteringInput,optInput,t,prevM,prevCov,computeLL)
+	  val (inferredMean,inferredCov,ll) = kalmanUpdate(filteringInput,optInput,t,prevM,prevCov,computeLL)
 	  hiddenMeans(::,t) := inferredMean
 	  hiddenCovs(t) = inferredCov
+	  logLikelihood = ll.map(_ + logLikelihood.get)
 	}
-	FilteringOutput(hiddenMeans,hiddenCovs,None)
+	FilteringOutput(hiddenMeans,hiddenCovs,logLikelihood)
   }
 
   private def kalmanUpdate(filteringInput:FilteringInput,optionalInput:OptionalInput,t:Int,
 						   prevMean:DenseVector[Double],prevCov:DenseMatrix[Double],computeLL:Boolean):
   	(DenseVector[Double],DenseMatrix[Double],Option[Double]) = {
 
+	val transMatrices = filteringInput.transMatrices.left.get
 	val (y,aMatrix,cMatrix,qNoise,rNoise) =
-	  (filteringInput.observations(::,t),filteringInput.A(t),filteringInput.C(t),filteringInput.Q(t),filteringInput.R(t))
+	  (filteringInput.observations(::,t),transMatrices.A(t),transMatrices.C(t),transMatrices.Q(t),transMatrices.R(t))
 	val (u,bMatrix,dMatrix) = (optionalInput.u(::,t),optionalInput.B(t),optionalInput.D(t))
+	val (obsSpaceSize,hiddenSpaceSize) = (y.length,aMatrix.rows)
+	assert(prevMean.length == hiddenSpaceSize && prevCov.rows == hiddenSpaceSize && prevCov.cols == hiddenSpaceSize)
+	assert(y.length == obsSpaceSize)
+	assert(bMatrix.rows == hiddenSpaceSize && bMatrix.cols == u.length)
+	assert(dMatrix.rows == obsSpaceSize && dMatrix.cols == u.length)
 	val (mPred:DenseVector[Double],covPred:DenseMatrix[Double]) = (t == 0) match {
 	  case true => (prevMean + (bMatrix * u),prevCov)
 	  case false =>
 		(aMatrix*prevMean + bMatrix*u,((aMatrix * prevCov) * aMatrix.t) + qNoise)
 	}
-	val stateSize = prevMean.length
 	val error:DenseVector[Double] = y - (cMatrix * mPred) - (dMatrix * u)
 	val sMatrix:DenseMatrix[Double] = ((cMatrix * covPred) * cMatrix.t) + rNoise
 	val sMatrixInv:DenseMatrix[Double] = inv(sMatrix)
 	val kalmanGainMtx:DenseMatrix[Double] = (covPred * cMatrix.t) * sMatrixInv
-	val temp1:DenseMatrix[Double] = (DenseMatrix.eye[Double](stateSize) - (kalmanGainMtx * cMatrix))
+	val temp1:DenseMatrix[Double] = (DenseMatrix.eye[Double](hiddenSpaceSize) - (kalmanGainMtx * cMatrix))
 	val logLikelihood = computeLL match {
 	  case true =>
 		Some(logGaussianDensity(error,means = DenseVector.zeros[Double](error.length),covs = sMatrix))
 	  case false =>	None
 	}
 	(mPred + (kalmanGainMtx * error),temp1 * covPred,logLikelihood)
+  }
+
+  private def generateOptionalInput(tMax:Int,hiddenSpaceSize:Int,obsSpaceSize:Int):OptionalInput = {
+
+	val (zeroBTransMatrix,zeroDTransMatrix) = (DenseMatrix.zeros[Double](hiddenSpaceSize,1),
+	  DenseMatrix.zeros[Double](obsSpaceSize,1))
+	val (arrWithZerosTransitionMatrix,arrWithZerosTransitionMatrix1) = (new Array[transitionMatrix](tMax),
+	  new Array[transitionMatrix](tMax))
+	(0 until tMax).foreach{index => arrWithZerosTransitionMatrix(index) = zeroBTransMatrix
+	  arrWithZerosTransitionMatrix1(index) = zeroDTransMatrix}
+	val (u,bMatrix,dMatrix) = (DenseMatrix.zeros[Double](1,tMax),
+	  arrWithZerosTransitionMatrix,arrWithZerosTransitionMatrix1)
+	OptionalInput(B = bMatrix,D = dMatrix,u = u)
+  }
+
+  private def stationaryModelToSeq(tMax:Int,stationaryMatrices:StationaryTransitionMatrices):TransitionsMatrices = {
+	val A:Array[transitionMatrix] = new Array[transitionMatrix](tMax)
+	val C:Array[transitionMatrix] = new Array[transitionMatrix](tMax)
+	val Q:Array[transitionMatrix] = new Array[transitionMatrix](tMax)
+	val R:Array[transitionMatrix] = new Array[transitionMatrix](tMax)
+
+	(0 until A.length).foreach(C(_) = stationaryMatrices.C)
+	(0 until C.length).foreach(A(_) = stationaryMatrices.A)
+	(0 until Q.length).foreach(Q(_) = stationaryMatrices.Q)
+	(0 until R.length).foreach(R(_) = stationaryMatrices.R)
+	TransitionsMatrices(A = A,C = C,Q = Q,R = R)
   }
 
 }
@@ -77,10 +118,16 @@ object KalmanFilter{
    * p(z1) = N(initMean,initCov)
    * observations(::,t) - observed state in step t
    */
-  case class FilteringInput(observations:DenseMatrix[Double],A:Array[transitionMatrix],
-							C:Array[transitionMatrix],Q:Array[transitionMatrix],R:Array[transitionMatrix],
-							 initMean:DenseVector[Double],initCov:DenseMatrix[Double])
 
+  case class FilteringInput(transMatrices:Either[TransitionsMatrices,StationaryTransitionMatrices],
+							observations:DenseMatrix[Double],
+								initMean:DenseVector[Double],initCov:DenseMatrix[Double])
+
+  case class TransitionsMatrices(A:Array[transitionMatrix],C:Array[transitionMatrix],
+								 Q:Array[transitionMatrix],R:Array[transitionMatrix])
+
+  case class StationaryTransitionMatrices(A:transitionMatrix,C:transitionMatrix,
+										  Q:transitionMatrix,R:transitionMatrix)
 
   case class OptionalInput(B:Array[transitionMatrix],D:Array[transitionMatrix],u:DenseMatrix[Double])
 
---
+++ src/main/scala/dynamicalsystems/tests/ObjectTrackingTest.scala
@@ -0,0 +1,51 @@
+package dynamicalsystems.tests
+
+import breeze.linalg.{DenseVector,DenseMatrix}
+import dynamicalsystems.filtering.KalmanFilter
+import dynamicalsystems.filtering.KalmanFilter.{StationaryTransitionMatrices, transitionMatrix, FilteringInput}
+
+/**
+ * Created by mjamroz on 30/03/14.
+ */
+
+/*
+ * z_t - hidden state from R^4 - (z1_t,z2_t,z1_t_vel,z2_t_vel)
+ * y_t - observed state from R^2 - (y1_t,y2_t)
+ * z_t = A*z_(t-1) + e_t
+ */
+class ObjectTrackingTest {
+
+  val timePeriod = 0.5
+
+  val aTransMatrix:DenseMatrix[Double] = DenseMatrix((1.,0.,timePeriod,0.),(0.,1.,0.,timePeriod),(0.,0.,1.,0.),(0.,0.,0.,1.))
+  val qNoise:DenseMatrix[Double] = DenseMatrix.eye[Double](4) :* 0.45
+  val cTransMatrix:DenseMatrix[Double] = DenseMatrix((1.,0.,0.,0.),(0.,1.,0.,0.))
+  val obs:DenseMatrix[Double] = DenseMatrix((8.,11.5,12.3,14.3,15.2,17.6,18.,18.7,20.2),
+											(11.,10.1,8.6,10.5,9.6,11.,8.2,8.5,9.5))
+
+  val rNoise:DenseMatrix[Double] = qNoise :* 1.4
+  val initMean = DenseVector(9.2,11.2,3.,1.)
+  val initCov = DenseMatrix.eye[Double](4) * 16.
+
+
+  def test = {
+	
+	val kalmanFilter = new KalmanFilter
+	val filteringInput = FilteringInput(observations = obs,initMean = initMean,initCov = initCov,
+	  transMatrices = Right(StationaryTransitionMatrices(A = aTransMatrix,C = cTransMatrix,Q = qNoise,R = rNoise)))
+	val filteringOutput = kalmanFilter.inferHiddenState(filteringInput,None,true)
+	(0 until filteringOutput.hiddenMeans.cols).foreach{index =>
+	  println(filteringOutput.hiddenMeans(::,index))
+	}
+	println(filteringOutput.logLikelihood)
+  } 
+
+}
+
+object ObjectTrackingTest {
+
+  def main(args:Array[String]) = {
+	(new ObjectTrackingTest).test
+  }
+}
+
