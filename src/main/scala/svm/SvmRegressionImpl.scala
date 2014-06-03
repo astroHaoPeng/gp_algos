42e779168294ae7729624b36eebfa9642f248510
Michal Jamroz <mjamroz90@gmail.com>
2014-06-03 19:23:29 +0200

    Svm regression with general kernel func.

--- src/main/scala/svm/SvmRegressionImpl.scala
+++ src/main/scala/svm/SvmRegressionImpl.scala
@@ -1,11 +1,10 @@
 package svm
 
 import gp.regression.GpPredictor.PredictionInput
-import utils.KernelRequisites.KernelFuncHyperParams
+import utils.KernelRequisites.{KernelFunc, KernelFuncHyperParams}
 import edu.berkeley.compbio.jlibsvm.regression.{EpsilonSVR, MutableRegressionProblemImpl}
 import breeze.linalg.DenseVector
 import edu.berkeley.compbio.jlibsvm.kernel.KernelFunction
-import gp.regression.Co2Prediction.Co2Kernel
 import edu.berkeley.compbio.jlibsvm.ImmutableSvmParameterPoint
 
 /**
@@ -13,19 +12,19 @@ import edu.berkeley.compbio.jlibsvm.ImmutableSvmParameterPoint
  */
 
 //TODO - make it invariant to kernelFunction
-class SvmRegressionImpl(kernelFunc:Co2Kernel) {
+class SvmRegressionImpl(kernelFunc:KernelFunc) {
 
   type objectType = DenseVector[Double]
   /*Svm returns just a value, not probabilistic distribution*/
-  def predict(input:PredictionInput,hyperParams:KernelFuncHyperParams=kernelFunc.hyperParams):DenseVector[Double] = {
+  def predict(input:PredictionInput,
+							   hyperParams:KernelFuncHyperParams=kernelFunc.hyperParams):DenseVector[Double] = {
 
 	val trainingDataSize = input.targets.length
 	val regressionProblemImpl = new MutableRegressionProblemImpl[objectType](trainingDataSize)
 	populateWithData(regressionProblemImpl,input)
-	val kernel = new JLibSvmCo2KernelFunction(kernelFunc)
 	val epsSvm = new EpsilonSVR[objectType,MutableRegressionProblemImpl[objectType]]
 	val builder = new ImmutableSvmParameterPoint.Builder[java.lang.Float,objectType]()
-	builder.kernel = kernel
+	builder.kernel = kernelFunc
 	builder.eps = 0.02f
 	val immutableSvmParam = new ImmutableSvmParameterPoint[java.lang.Float,objectType](builder)
 	val regressionModel = epsSvm.train(regressionProblemImpl,immutableSvmParam)
@@ -42,13 +41,16 @@ class SvmRegressionImpl(kernelFunc:Co2Kernel) {
 	}
   }
 
-  class JLibSvmCo2KernelFunction(gpCo2KernelFunc:Co2Kernel) extends KernelFunction[objectType]{
+  class JLibSvmKernel(kernelFunc:KernelFunc) extends KernelFunction[objectType] {
 
 	override def evaluate(x: objectType, y: objectType): Double = {
-	  	/*Rather bad, but in the case of Co2 prediction it is enough*/
-	  	val sameIndex = x.equals(y)
-		gpCo2KernelFunc.apply(x,y,sameIndex)
+		val sameIndex = x.equals(y)
+	    kernelFunc.apply(x,y,sameIndex)
 	}
+  }
+
+  implicit def gpToSvmCo2Kernel(gpKernel:KernelFunc):KernelFunction[objectType] = {
+	new JLibSvmKernel(kernelFunc)
   }
 
 }
